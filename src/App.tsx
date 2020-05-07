7582503048f062ebc24493d49eb080f9b47e2872
Juan Belieni <juanbelieni@gmail.com>
2020-05-06 21:22:39 -0300

    Delete unnecessary pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,8 @@ import {
   Route,
 } from 'react-router-dom';
 
-import AddPpc from './pages/AddPpc';
-import AddTurma from './pages/AddTurma';
-import EditPpc from './pages/EditPpc';
 import PpcsTable from './pages/PpcsTable';
 import Relatorios from './pages/Relatorios';
-import SetCarga from './pages/SetCarga';
 import ShowPpc from './pages/ShowPpc';
 import ShowTurma from './pages/ShowTurma';
 import Tabelas from './pages/Tabelas';
@@ -34,18 +30,9 @@ const App : React.FC = () => (
         <PpcsTable />
       </Route>
 
-      <Route path="/tabelas/ppcs/adicionar" exact>
-        <AddPpc />
-      </Route>
-
       <Route path="/tabelas/ppcs/:id" exact>
         <ShowPpc />
       </Route>
-
-      <Route path="/tabelas/ppcs/:id/editar" exact>
-        <EditPpc />
-      </Route>
-
 
       <Route path="/tabelas/professores" exact>
         <Tabelas table="professores" />
@@ -55,16 +42,8 @@ const App : React.FC = () => (
         <TurmasTable />
       </Route>
 
-      <Route path="/tabelas/turmas/adicionar" exact>
-        <AddTurma />
-      </Route>
-
       <Route path="/tabelas/turmas/:id" exact>
         <ShowTurma />
-      </Route>
-
-      <Route path="/tabelas/turmas/:turma_id/:disciplina_id" exact>
-        <SetCarga />
       </Route>
 
       <Route path="/relatorios" exact>
